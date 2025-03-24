036d0de515bf9fe28336244259402be9e0eb338d
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-03-25 00:02:22 +0530

    update

--- server/templates/adminOrderPlaced.js
+++ server/templates/adminOrderPlaced.js
@@ -103,8 +103,7 @@ const adminOrderNotificationTemplate = (order) => {
                 <p><span class="highlight">Order Number:</span> #${order_number}</p>
                 <p><span class="highlight">Estimated Delivery:</span> ${new Date(estimated_delivery).toDateString()}</p>
                 <p><span class="highlight">Payment Status:</span> ${payment_status}</p>
-                <p><span class="highlight">Shipping Method:</span> ${shipping_method}</p>
-                <p><span class="highlight">Total Amount:</span> $${total_amount.toFixed(2)}</p>
+                        <p><span class="highlight">Total Amount:</span> $${total_amount.toFixed(2)}</p>
     
                 <h3>🛍 Ordered Items:</h3>
                 <table class="product-table">

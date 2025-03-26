8cc392a19b511c70216db697c4721f0ae6f213a4
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-03-26 23:44:26 +0530

    update button ficx

--- src/components/invoices/components/InvoiceFilters.tsx
+++ src/components/invoices/components/InvoiceFilters.tsx
@@ -37,7 +37,7 @@ export function InvoiceFilters({ onFilterChange,exportInvoicesToCSV }: InvoiceFi
 
   return (
     <div className="space-y-4 ">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <Input
           type="text"
           placeholder="Search invoice Number"

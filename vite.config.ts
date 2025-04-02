29feeb1021cea94786f70d1ffba096a139a5637a
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-04-02 22:25:42 +0530

    remove csp

--- vite.config.ts
+++ vite.config.ts
@@ -29,19 +29,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' https://js.authorize.net https://maps.googleapis.com; style-src 'self' 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://maps.googleapis.com; connect-src 'self' https://cfyqeilfmodrbiamqgme.supabase.co https://maps.googleapis.com;"
-          }
-        ]
-      }
-    ];
-  }
-};

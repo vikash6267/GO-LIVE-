d0e870ab0fda92ff8d8fff030409f9ba905e785c
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-04-03 12:08:18 +0530

    update remove authorize and headers in vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,6 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 3000,
-       headers: {
-        "X-Content-Type-Options": "nosniff",
-        "Content-Security-Policy": "default-src 'self'",
-        "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
-        "Referrer-Policy": "no-referrer"
-    }
   },
   plugins: [
     react(),
